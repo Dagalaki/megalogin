a6cbfa80d47bd1bc26321fe2c40a5cfa98922df6
edagalaki <edagalaki@gmail.com>
2024-04-05 15:08:23 +0300

    hide header links if no action

--- index.php
+++ index.php
@@ -66,7 +66,9 @@ function popup(mylink, windowname) {
 			<img src="img/logo2.png" />
 		</a>
 		<!--a class="signin" href="login.php">Είσοδος</a-->
-		<div class="links"><div><a href="login-tv.php">ΕΙΣΟΔΟΣ ΣΤΗΝ ΤΗΛΕΟΡΑΣΗ</a></div><div><a class="active" href="?action=edit">Ο ΛΟΓΑΡΙΑΣΜΟΣ ΜΟΥ</a></div>
+		<div class="links">
+<?php if($action){ ?>
+			<div><a href="login-tv.php">ΕΙΣΟΔΟΣ ΣΤΗΝ ΤΗΛΕΟΡΑΣΗ</a></div><div><a class="active" href="?action=edit">Ο ΛΟΓΑΡΙΑΣΜΟΣ ΜΟΥ</a></div>
 		<div id="circles">
 			
       <svg id="more-options" xmlns="http://www.w3.org/2000/svg" width="25" height="7" viewBox="0 0 25 7">
@@ -96,7 +98,7 @@ function popup(mylink, windowname) {
     	</div>
 		</div>
 	</div>
-
+<?php } ?>
 	</header>
 
 	<div class="outerform">
